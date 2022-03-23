ce9105546d0ee3f3839e95cf61fe363c7496f868
yjpyjp123 <aptl741741@daum.net>
2022-03-23 19:16:01 +0900

    Create project

---
+++ .postcssrc.js
@@ -0,0 +1,16 @@
+// ESM
+// CommonJS
+
+// import autoprefixer from 'autoprefixer'
+// const autoprefixer = require('autoprefixer')
+
+// export {
+//  piugins: [
+//    autoprefixer
+//  ]
+//}
+module.exports = {
+  piugins: [
+    require('autoprefixer')
+  ]
+}
